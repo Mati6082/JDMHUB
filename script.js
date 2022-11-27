c25070e1eb2aba59e20fea42fa8a576a8fbdf7fd
Mati6082 <matpakla@gmail.com>
2022-11-27 16:06:54 +0100

    menu działa!!!!!

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName('menu-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-item')[0]
+const navbarLinks = document.getElementsByClassName('menu')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
